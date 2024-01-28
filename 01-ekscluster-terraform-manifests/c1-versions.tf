7e889ed718765fb4dd9ac836b435a869d488f51a
LokeshJavvaji <lokeshjavvaji94@gmail.com>
2024-01-28 15:54:47 +0530

    files uploaded

---
+++ 01-ekscluster-terraform-manifests/c1-versions.tf
@@ -0,0 +1,24 @@
+# Terraform Settings Block
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"     
+      version = ">= 5.31"      
+     }
+  }
+  # Adding Backend as S3 for Remote State Storage
+  backend "s3" {
+    bucket = "terraform-awseks"
+    key    = "prod/eks-cluster/terraform.tfstate"
+    region = "us-east-1" 
+ 
+    # For State Locking
+    dynamodb_table = "prod-ekscluster"    
+  }  
+}
+
+# Terraform Provider Block
+provider "aws" {
+  region = var.aws_region
+}
---
+++ 01-ekscluster-terraform-manifests/c4-07-ec2bastion-provisioners.tf
@@ -0,0 +1,14 @@
+# Create a Null Resource and Provisioners
+resource "null_resource" "copy_ec2_keys" {
+  depends_on = [module.ec2_public]
+  # Connection Block for Provisioners to connect to EC2 Instance
+  connection {
+    type     = "ssh"
+    host     = aws_eip.bastion_eip.public_ip    
+    user     = "ec2-user"
+    password = ""
+    private_key = file("private-key/kubernetes.pem")
+  }  
+
+
+}
